47a3c05a6309e75f2a507e9d73fa698cd72b13c5
denkhaus <peristaltic@gmx.net>
2017-08-28 00:24:40 +0200

    work in progress

--- objects/assetamount.go
+++ objects/assetamount.go
@@ -12,38 +12,12 @@ type AssetAmount struct {
 	Amount UInt64     `json:"amount"`
 }
 
-func (p *AssetAmount) Rate(prec int) float64 {
-	return float64(p.Amount) / math.Pow(10, float64(prec))
+func (p *AssetAmount) Rate(prec float64) float64 {
+	return float64(p.Amount) / math.Pow(10, prec)
 }
 
-//Add adds two asset amounts. They must refer to the same Asset type.
-//other: The other AssetAmount to add to this.
-//return: The same instance of the AssetAmount class with the combined amount.
-func (p *AssetAmount) Add(other AssetAmount) *AssetAmount {
-	if p.Asset.Id() != other.Asset.Id() {
-		panic("Cannot add two AssetAmount instances that refer to different assets")
-	}
-
-	p.Amount += other.Amount
-	return p
-}
-
-//Subtract subtracts another instance of AssetAmount from this one. This method will always
-//return absolute values.
-//other: The other asset amount to subtract from this.
-//return: The same instance of the AssetAmount class with the combined amount.
-func (p *AssetAmount) Subtract(other AssetAmount) *AssetAmount {
-	if p.Asset.Id() != other.Asset.Id() {
-		panic("Cannot subtract two AssetAmount instances that refer to different assets")
-	}
-
-	if p.Amount > other.Amount {
-		p.Amount -= other.Amount
-	} else {
-		p.Amount = other.Amount - p.Amount
-	}
-
-	return p
+func (p AssetAmount) Valid() bool {
+	return p.Asset.Valid() && p.Amount != 0
 }
 
 //implements Operation interface
--- objects/price.go
+++ objects/price.go
@@ -5,6 +5,10 @@ type Price struct {
 	Quote AssetAmount `json:"quote"`
 }
 
-func (p Price) Rate(precBase, precQuote int) float64 {
+func (p Price) Rate(precBase, precQuote float64) float64 {
 	return p.Base.Rate(precBase) / p.Quote.Rate(precQuote)
+}
+
+func (p Price) Valid() bool {
+	return p.Base.Valid() && p.Quote.Valid()
 }
