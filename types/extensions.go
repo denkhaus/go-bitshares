a4e0e7394455670a5629a41e00c32294a7c168be
denkhaus <peristaltic@gmx.net>
2019-04-10 11:10:24 +0200

    generate ffjson

--- types/extensions.go
+++ types/extensions.go
@@ -11,26 +11,25 @@ import (
 
 type Extensions json.RawMessage
 
+//TODO refactor and test
 func (p Extensions) Marshal(enc *util.TypeEncoder) error {
-	if err := enc.EncodeUVarint(uint64(len(p))); err != nil {
+	if err := enc.EncodeUVarint(uint64(0)); err != nil {
 		return errors.Annotate(err, "encode length")
 	}
 
-	for _, ex := range p {
-		if err := enc.Encode(ex); err != nil {
-			return errors.Annotate(err, "encode Extension")
-		}
-	}
+	// if err := enc.EncodeUVarint(uint64(len(p))); err != nil {
+	// 	return errors.Annotate(err, "encode length")
+	// }
+
+	// for _, ex := range p {
+	// 	if err := enc.Encode(ex); err != nil {
+	// 		return errors.Annotate(err, "encode Extension")
+	// 	}
+	// }
 
 	return nil
 }
 
-// func (p *Extensions) UnmarshalJSON(s []byte) error {
-// 	var val interface{}
-// 	if err := ffjson.Unmarshal(s, &val); err != nil {
-// 		return errors.Annotate(err, "Unmarshal")
-// 	}
-
-// 	fmt.Printf("extension %+v", val)
-// 	return nil
-// }
+func (p *Extensions) UnmarshalJSON(s []byte) error {
+	return p.UnmarshalJSON(s)
+}
